1143c06b1802cf8c97b33d45715f7b5f08662104
Artem Levin <leoartemlev@yandex.ru>
2022-10-06 15:10:38 +0500

    first endpoint (for shorting url)

---
+++ UrlShorter/UrlShorter/Controllers/UrlController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UrlShorter.Core;
+
+namespace UrlShorter.Controllers;
+
+public class UrlController : Controller
+{
+    private readonly TokenService _tokenService;
+
+    public UrlController(TokenService tokenService)
+    {
+        _tokenService = tokenService;
+    }
+
+    [HttpPost("/short/url")]
+    public IActionResult ShortUrl([FromBody] string longUrl)
+    {
+        var token = _tokenService.GenerateTokenFor(longUrl);
+        return Ok($"https://{HttpContext.Request.Host}/{token}");
+    }
+}
---
+++ UrlShorter/UrlShorter/Core/MockEncryptValueGiver.cs
@@ -0,0 +1,18 @@
+﻿using UrlShorter.Core.Abstractions;
+
+namespace UrlShorter.Core;
+
+public class MockEncryptValueGiver : IEncryptValueGiver
+{
+    private int _max = 1000;
+    private int _current = 0;
+    public long GetValue()
+    {
+        if (_current > _max)
+        {
+            _current = 0;
+        }
+
+        return _current++;
+    }
+}
